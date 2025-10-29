eea98663ff3419a34deec823e62b4ba8b1254f70
Victor <balogunaminat2017@gmail.com>
2025-10-29 23:07:18 +0100

    about build

---
+++ ASSETS/javascript/index.js
@@ -0,0 +1,9 @@
+var navbar = document.querySelector("#navbar");
+window.addEventListener("scroll", function () {
+    if(window.scrollY > 100) {
+        navbar.classList.add("scrolled");
+    }
+    else{
+        navbar.classList.remove("scrolled");
+    }
+})
